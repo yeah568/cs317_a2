0513e658835ea966fe81bd0eae50d9465b0d1a8c
James Liu <jamesjhliu@gmail.com>
2016-11-05 19:05:04 -0700

    more work

--- DNSlookup.java
+++ DNSlookup.java
@@ -21,6 +21,11 @@ public class DNSlookup {
 
     static DatagramSocket socket;
 
+
+	static DNSMessage firstQuery;
+	static int queryCount = 0;
+
+
 	/**
      * @param args
 	 */
@@ -51,50 +56,17 @@ public class DNSlookup {
 
 
         DNSMessage msg = new DNSMessage(fqdn);
+		firstQuery = msg;
 		sendMessage(msg, rootNameServer);
-
-		// TODO: check  length
-		byte[] recvBuf = new byte[512];
-		DatagramPacket recv = new DatagramPacket(recvBuf, recvBuf.length);
-		try {
-			socket.receive(recv);
-		} catch (SocketTimeoutException e) {
-			handleError(-2, msg);
-		}
-
-		DNSMessage recvMsg = new DNSMessage(recv.getData());
-
-		if (tracingOn) {
-			recvMsg.dumpResponse();
-		}
-
-		if (recvMsg.getHeader().getReplyCode() == 3) {
-			handleError(-1, msg);
-		} else if (recvMsg.getHeader().getReplyCode() != 0) {
-			handleError(-4, msg);
-		}
-
-		if (recvMsg.getAnswers().size() > 0) {
-			// one or more answers
-			switch (recvMsg.getAnswers().get(0).type) {
-				case A:
-					break;
-				case AAAA:
-					break;
-				case CN:
-					break;
-			}
-		}
-
-
-
+		receiveMessage();
 	}
 
-	private static void sendMessage(DNSMessage message, InetAddress ns) {
+	private static void sendMessage(DNSMessage message, InetAddress ns) throws IOException {
+		if (query)
+
 		if (tracingOn) {
-			System.out.println("\n\n");
 			System.out.println(String.format(
-					"Query ID     %s %s --> %s",
+					"\n\nQuery ID     %s %s --> %s",
 					Integer.toUnsignedString(message.getHeader().getQueryID()),
 					message.getQuestions().get(0).getName(),
 					ns.getHostAddress()
@@ -104,11 +76,81 @@ public class DNSlookup {
 		byte[] buf = message.getBuffer();
 
 		DatagramPacket packet = new DatagramPacket(buf, buf.length, ns, 53);
-		try {
-			socket.send(packet);
-		} catch (IOException e) {
-			e.printStackTrace();
+		socket.send(packet);
+	}
+
+	private static void receiveMessage() throws IOException {
+		byte[] recvBuf = new byte[512];
+		DatagramPacket recv = new DatagramPacket(recvBuf, recvBuf.length);
+
+		socket.receive(recv);
+
+		DNSMessage recvMsg = new DNSMessage(recv.getData());
+
+		if (tracingOn) {
+			recvMsg.dumpResponse();
 		}
+
+		if (recvMsg.getHeader().getReplyCode() == 3) {
+			handleError(-1, firstQuery);
+		} else if (recvMsg.getHeader().getReplyCode() != 0) {
+			handleError(-4, firstQuery);
+		}
+
+		if (recvMsg.getAnswers().size() > 0) {
+			// one or more answers
+			RR firstAnswer = recvMsg.getAnswers().get(0);
+			switch (firstAnswer.type) {
+				case A:
+					break;
+				case AAAA:
+					break;
+				case CN:
+					// CNAME, need to resolve
+					DNSMessage msg = new DNSMessage(firstAnswer.data.toString());
+					sendMessage(msg, rootNameServer);
+					receiveMessage();
+					break;
+			}
+		} else {
+			// no answers, need to go deeper
+			if (recvMsg.getAuthorities().size() > 0) {
+				// get next nameserver to check
+				String ns = recvMsg.getAuthorities().get(0).data.toString();
+
+				// check if nameserver IP is given in additional info
+				if (recvMsg.getAdditional().size() > 0) {
+					for (RR additional : recvMsg.getAdditional()) {
+						if (additional.name.equalsIgnoreCase(ns) && additional.type == RRType.A) {
+							String nextNS = additional.data.toString();
+
+							DNSMessage msg = new DNSMessage(firstQuery.getQuestions().get(0).getName());
+							// NOTE: this does not do DNS resolution. If it's given an IP address string, it only
+							// converts it to an InetAddress.
+							sendMessage(msg, InetAddress.getByName(nextNS));
+							receiveMessage();
+							break;
+						}
+					}
+				}
+
+				// IP address not given, resolve name server IP.
+
+
+			}
+		}
+	}
+
+
+	private DNSMessage resolveName(String host, InetAddress ns) {
+		DNSMessage msg = new DNSMessage(host);
+		try {
+			sendMessage(msg, ns);
+		} catch (IOException e) {
+			handleError(-4, msg);
+		}
+
+
 	}
 
 	private static void handleError(int code, DNSMessage message) {
